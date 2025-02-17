c2777eb4c931329f15de83857db0e30c31d76398
Daniela2319 <frankvt04@gmail.com>
2025-02-17 14:22:06 -0300

    Criado a classe cliente

---
+++ Agendamento/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agendamento
+{
+    public class Cliente
+    {
+        public string Name { get; set; }
+        public string Celular { get; set; }
+
+        public Cliente(string nome, string celular) 
+        {
+            Name = nome;
+            Celular = celular;
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Cliente: {Name}, Celular: {Celular}");
+        }
+
+        public override string ToString()
+        {
+            return $"Cliente: {Name}, Celular: {Celular}";
+        }
+    }
+}
